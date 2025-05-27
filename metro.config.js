8f4897294e3f47304147e4de5c9a915909b79dd5
OluRemiFour <lekanayoola04@gmail.com>
2025-05-27 17:43:54 +0100

    updates

--- metro.config.js
+++ metro.config.js
@@ -1,12 +1,11 @@
-/**
- * Metro configuration for React Native
- * https://github.com/facebook/react-native
- *
- * @format
- */
+const { getDefaultConfig } = require('@expo/metro-config');
+
+const defaultConfig = getDefaultConfig(__dirname);
 
 module.exports = {
+  ...defaultConfig,
   transformer: {
+    ...defaultConfig.transformer,
     getTransformOptions: async () => ({
       transform: {
         experimentalImportSupport: false,
